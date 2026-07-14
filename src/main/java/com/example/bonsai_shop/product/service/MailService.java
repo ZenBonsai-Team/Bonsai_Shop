e6112312317675aeea1f78f1e4aa45a1f5d27d06
DuongNKT <nguyenkieutungduong@gmail.com>
2026-07-14 23:04:30 +0700

    fix(ui): resolve Thymeleaf parsing error (HTTP 500) on Admin and Seller dashboards - Declared `paramActivePage` parameter in the `sidebar` fragment signature. - Dynamically resolved `role` and `activePage` within `sidebar.html` to support all dashboard callers. - Fixed the template rendering crash when navigating to /admin or /seller.

--- src/main/java/com/example/bonsai_shop/product/service/MailService.java
+++ src/main/java/com/example/bonsai_shop/product/service/MailService.java
@@ -1,4 +1,16 @@
 package com.example.bonsai_shop.product.service;
 
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class MailService {
+    /*
+    * 1. Nhận yêu cầu gửi mail cho đơn hàng
+    * 2. Kiểm tra Suppression Rules: Nếu đơn hàng có yêu cầu gửi trùng lặp trong vòng 60s
+    * yêu cầu t2 sẽ bị bỏ quá
+    * 3. Thiết lập nội dung HTML theo mẫu thiết kế TEMPLATE ID: TMP-ORD-CONF
+    * 4. Áp dụng Retry PolicyL Nếu gửi lỗi, thử lại tối đa 3 lần, mỗi lần cách nhau 2s
+    * */
+//    private final JavaMailSender mailSender;
+
+
 }
