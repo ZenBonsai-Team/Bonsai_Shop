0724319cda4d7a57c2c003426cb3547d73fb35c9
hiepbt304 <hiepbthe182078@fpt.edu.vn>
2026-08-04 22:48:56 +0700

    feat: add wishlist

--- src/main/java/com/example/bonsai_shop/product/dto/WishlistItemResponseDTO.java
+++ src/main/java/com/example/bonsai_shop/product/dto/WishlistItemResponseDTO.java
@@ -1,25 +1,28 @@
 package com.example.bonsai_shop.product.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ProductDetailResponseDTO {
+public class WishlistItemResponseDTO {
     private Integer productId;
     private String productCode;
     private String productName;
-    private String description;
-    private Integer age;
-    private Float height;
-    private Float trunkDiameter;
-    private String style;
+    private String productImage;
     private BigDecimal price;
     private String productStatus;
     private Boolean isVisible;
     private Integer segmentId;
     private String segmentName;
-    private String imageUrl;
+    private LocalDateTime createdAt;
+    private Boolean canAddToCart;
+    private String detailUrl;
 }
